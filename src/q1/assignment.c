3cc04780e13ee8282b9fd407deb219b271a590f6
Eoin Rockall <e.rockall9@gmail.com>
2018-10-22 20:22:45 +0100

    Question 1
    Code for Question 1 of Assignment 1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+int i;
+int* in;
+long lng;
+double* doub;
+char** c;
+
 int main(int arg, char* argc[]){
   printf("Hello assignment1.\n");
+
+  printf("%d\n", sizeof(i));
+  printf("%d\n", sizeof(in));
+  printf("%d\n", sizeof(lng));
+  printf("%d\n", sizeof(doub));
+  printf("%d\n", sizeof(c));
   return 0;
 }
